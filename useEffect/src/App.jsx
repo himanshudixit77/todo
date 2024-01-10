9a83b63b4f0c08fbff1e5c6541beaf1e98d3aacc
himanshudixit <hdixit683@gmail.com>
2024-01-11 00:43:36 +0530

    UseEffect other conditon also

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -5,9 +5,19 @@ import './App.css'
 function App() {
   const [count1, setCount1] = useState(0)
   const [count2, setCount2] = useState(0)
+  //To run useEffect only once on the first render pass any empty array in the dependecy
   useEffect(() => {
   alert("clicked")
-})
+  }, [])
+  
+  //To run useEffect on change of a particular value. Pass the state and props in the dependency array
+  useEffect(() => {
+    alert("clicked")
+  }, [count1, count2])
+  //only click1 is oressed then only alert box is seen
+  useEffect(() => {
+    alert("clicked")
+  },[count1])
 
   const handleClick1 = () => {
   setCount1(count1 +1)
