133451280c3a696024463c51d0db27ef5dff089d
himanshudixit <hdixit683@gmail.com>
2024-01-11 15:20:54 +0530

    changes dyanicmic

--- useEffect/src/Header.jsx
+++ useEffect/src/Header.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react";
 
 
- export function Header() {
+export function Header() {
+    const [title, setTitle] = useState('')
+  //  const[TitleList, setList] = useState([ ])
+    
+    const handleClick = () => {
+        setTitle("my name is " + Math.random())
+        
+       // let newtitle = [title +" ", ...TitleList]
+        //setList(newtitle)
+
+        
+    }
+
     return (
         <div>
-        <Name prop="himanshu" />
-            <Name prop="dixit" />
-            <SurName surname={{ name: "mishra", age: 25 }}></SurName>
+            <button style={{
+                height:20,
+                width:60
+            }}type="Submit" onClick={handleClick}>Click</button>
+         
+            <Name prop={title}></Name>
+            {<Name prop="dixit" />}
+            {/*<Name prop={TitleList}></Name><br></br>*/}
+            {/*<SurName surname={{ name: "mishra", age: 25 }}></SurName> */}
        
         </div>
   )
